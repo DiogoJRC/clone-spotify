7fa1b1d0367765036f319ca0d03bf555fae68da8
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-13 21:41:13 -0300

    install `react-router-dom` and create `Home` page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
-import Main from "./components/Main";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 import spotifyLogo from "../assets/logo/spotify-logo.png";
 
 const Header = () => {
   return (
     <div className="header">
-      <a href="">
+      <Link to="/">
         <img src={spotifyLogo} alt="" />
-      </a>
-      <a href="/">
+      </Link>
+      <Link to="/">
         <h1>Spotify</h1>
-      </a>
+      </Link>
     </div>
   );
 };
