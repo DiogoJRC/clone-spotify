e169f127086cfcaffd1659931806a5982f9049dc
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-15 11:22:10 -0300

    install and config `cors`

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const { db } = require("./connect.js");
 
 const app = express();
 const port = 5174;
+
+app.use(cors());
 
 app.get("/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
