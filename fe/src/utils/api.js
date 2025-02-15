74de0a09eb4c004453317066697a2baa3b6826a9
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-15 11:47:39 -0300

    create function to shuffle `artists` and `songs` arrays

--- fe/src/utils/api.js
+++ fe/src/utils/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import shuffle from "./shuffle";
 
 const url = "http://localhost:5174";
 
 const artistsResponse = await axios.get(`${url}/artists`);
 const songsResponse = await axios.get(`${url}/songs`);
 
-export const artists = artistsResponse.data;
-export const songs = songsResponse.data;
+export const artists = shuffle(artistsResponse.data);
+export const songs = shuffle(songsResponse.data);
